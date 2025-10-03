2a469dd105db5461d43da8a581d8eda0c30b7d97
fendifrost-dot <fendifrost@gmail.com>
2025-10-03 12:59:16 -0500

    Fix calendar component icon structure

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -54,8 +54,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        Icon: {
+          Left: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
+          Right: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        },
       }}
       {...props}
     />
